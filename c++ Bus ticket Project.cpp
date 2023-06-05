1a521ebb245bbd6ab8f5706628acbe01436c5c7c
Biniam Girma <132125339+bina-gi@users.noreply.github.com>
2023-06-05 19:37:50 +0300

    Update c++ Bus ticket Project.cpp

--- c++ Bus ticket Project.cpp
+++ c++ Bus ticket Project.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 int main()
 {
-	string full_name;
+	string f_name;
+	string l_name;
 	int dep;
 	int dis;
+start:	
 	cout<<"      ============================================================"<<endl;
 	cout<<"            WELL COME TO OUR BUS TICKET RESERVATION SOFTWARE       "<<endl;
 	cout<<"      ============================================================"<<endl;
@@ -13,7 +15,7 @@ int main()
 	cout<<"\n Dear customer Please fill all asked informations below CORRECTLY"<<endl;
 	cout<<"---------------------------------------------------------------------------"<<endl;
 	cout<<"Enter Your full Neme: ";
-	cin>>full_name;
+	cin>>f_name; cin>>l_name;
 new1:	cout<<"Please select your departure"<<endl;
 		cout<<"  1. Addis Abeba"<<endl;
 		cout<<"  2. Bahir Dar"<<endl;
@@ -25,71 +27,148 @@ new1:	cout<<"Please select your departure"<<endl;
 		cout<<"  8. Jimma"<<endl;
 		cout<<"Enter the number that hold your Departure: "; 
 		cin>>dep;	
-		if( dep>7 || dep<1 )
+		if( dep>8||dep<1 )
 		{
-			cout<<"Unkown Departure!!!"<<"\n ********************************************************************************"<<"\n Please enter Your CORRECT Departure again"<<endl;
-			goto new1;
+                    cout<<"Unkown Departure!!!"<<"\n ********************************************************************************"<<"\n Please enter Your CORRECT Departure again"<<endl;
+                    goto new1;
 		}
 	if(dep==1)
 	{
 		cout<<"Please select your Distiniction"<<endl;
-		cout<<"  1. Bahir Dar"<<endl;
-		cout<<"  2. Hawasa"<<endl;
-		cout<<"  3. Gonder"<<endl;
-		cout<<"  4. Desie"<<endl;
-		cout<<"  5. Semera"<<endl;
-		cout<<"  6. Dire Dawa"<<endl;
-		cout<<"  7. Jimma"<<endl;
+		cout<<"  10. Bahir Dar"<<endl;
+		cout<<"  11. Hawasa"<<endl;
+		cout<<"  12. Gonder"<<endl;
+		cout<<"  13. Desie"<<endl;
+		cout<<"  14. Semera"<<endl;
+		cout<<"  15. Dire Dawa"<<endl;
+		cout<<"  16. Jimma"<<endl;
 		cout<<"Enter the number that hold your Distinction"<<endl;
 		cin>>dis;
 	}
 	else if(dep != 1)
 	{
-		cout<<"your Distinction is Addis Abeba";
+			cout<<endl;
+                dis=0;
 	}
-	switch(dep,dis){
-		case 1&&1:
-			cout<<"your Travel is"<<endl;
+	if(dep==1 && dis==1)
+			{
+            cout<<"your Travel is"<<endl;
 			cout<<"From ADDIS ABEBA"<<endl;
-			cout<<"To BAHIR DAR"<<endl
-			cout<<"It is 496.5KM it takes 9:45 hrs ";
-			cout<<"It costs 1300 ETB";
-		case 1&&2:
-			cout<<"your Travel is"<<endl;
+			cout<<"To BAHIR DAR"<<endl;
+			cout<<"It is 496.5KM it takes 9:45 hrs. "<<endl;
+			cout<<"It costs 1300 ETB"<<endl;
+                        }
+            else if(dep==1 && dis==2)
+			{
+                        cout<<"your Travel is"<<endl;
 			cout<<"From ADDIS ABEBA"<<endl;
-			cout<<"To HAWASA"<<endl
-			cout<<"It is 496.5KM it takes 9:45 hrs ";
-			cout<<"It costs 1300 ETB";
-		case 1&&3:
-			cout<<"your Travel is"<<endl;
+			cout<<"To HAWASA"<<endl;
+			cout<<"It is 300KM it takes 5:30 hrs "<<endl;
+			cout<<"It costs 700 ETB"<<endl;
+                        }
+            else if(dep==1 && dis==3)
+			{
+                        cout<<"your Travel is"<<endl;
 			cout<<"From ADISS ABEBA"<<endl;
-			cout<<"To GONDER"<<endl
-			cout<<"It is 496.5KM it takes 9:45 hrs ";
-			cout<<"It costs 1300 ETB";
-		case 1&&4:
+			cout<<"To GONDER"<<endl;
+			cout<<"It is 590KM it takes 11:30 hrs "<<endl;
+			cout<<"It costs 1500 ETB"<<endl;
+                        }
+            else if(dep==1 && dis==4)
+                        {j
 			cout<<"your Travel is"<<endl;
 			cout<<"From ADDIS ABEBA"<<endl;
-			cout<<"To DESIE"<<endl
-			cout<<"It is 496.5KM it takes 9:45 hrs ";
-			cout<<"It costs 1300 ETB";
-		case 1&&5:
+			cout<<"To DESIE"<<endl;
+			cout<<"It is 400KM it takes 6:50 hrs "<<endl;
+			cout<<"It costs 900 ETB"<<endl;
+                        }
+            else if(dep==1 && dis==5)
+                        {
 			cout<<"your Travel is"<<endl;
 			cout<<"From ADDIS ABEBA"<<endl;
-			cout<<"To SEMERA"<<endl
-			cout<<"It is 496.5KM it takes 9:45 hrs ";
-			cout<<"It costs 1300 ETB";
-		case 1&&6:
-				cout<<"your Travel is"<<endl;
+			cout<<"To SEMERA"<<endl;
+			cout<<"It is 530KM it takes 10:50 hrs "<<endl;
+			cout<<"It costs 1550 ETB"<<endl;
+                        }
+            else if(dep==1 && dis==6)
+			{
+            cout<<"your Travel is"<<endl;
 			cout<<"From ADDIS ABEBA"<<endl;
-			cout<<"To DIRE DAWA"<<endl
-			cout<<"It is 496.5KM it takes 9:45 hrs ";
-			cout<<"It costs 1300 ETB";
-		case 1&&2:
-		case 1&&2:
-		case 1&&2:
-		case 1&&2:
-	}
+			cout<<"To DIRE DAWA"<<endl;
+			cout<<"It is 453.5KM it takes 10:10 hrs "<<endl;
+            cout<<"It costs 1450 ETB"<<endl;
+                        }
+            else if(dep==1 && dis==7)
+			{
+            cout<<"your Travel is"<<endl;
+			cout<<"From ADDIS ABEBA"<<endl;
+			cout<<"To JIMMA"<<endl;
+			cout<<"It is 500KM it takes 10:00 hrs "<<endl;
+            cout<<"It costs 1550 ETB"<<endl;
+                        }
+            else if(dep==2 && dis==0)
+			{
+            cout<<"your Travel is"<<endl;
+			cout<<"From BAHIR DAR"<<endl;
+			cout<<"To ADDIS ABEBA"<<endl;
+			cout<<"It is 496.5KM it takes 9:45 hrs "<<endl;
+			cout<<"It costs 1300 ETB"<<endl;
+                        }
+            else if(dep==3 && dis==0)
+			{
+            cout<<"your Travel is"<<endl;
+			cout<<"From HAWASA"<<endl;
+			cout<<"To ADDIS ABEBA"<<endl;
+			cout<<"It is 300KM it takes 5:30 hrs "<<endl;
+			cout<<"It costs 700 ETB"<<endl;
+            }
+            else if(dep==4 && dis==0)
+			{
+            cout<<"your Travel is"<<endl;
+			cout<<"From GONDER"<<endl;
+			cout<<"To ADISS ABEBA"<<endl;
+			cout<<"It is 590KM it takes 11:30 hrs "<<endl;
+			cout<<"It costs 1500 ETB"<<endl;
+                        }
+            else if(dep==5 && dis==0)
+                        {
+			cout<<"your Travel is"<<endl;
+			cout<<"From DESIE"<<endl;
+			cout<<"To ADDIS ABEBA"<<endl;
+			cout<<"It is 400KM it takes 6:50 hrs "<<endl;
+			cout<<"It costs 900 ETB"<<endl;
+                        }
+            else if(dep==6 && dis==0)
+                        {
+			cout<<"your Travel is"<<endl;
+			cout<<"From SEMERA"<<endl;
+			cout<<"To ADDIS ABEBA"<<endl;
+			cout<<"It is 530KM it takes 10:50 hrs "<<endl;
+			cout<<"It costs 1550 ETB"<<endl;
+                        }
+            else if(dep==7 && dis==0)
+			{
+            cout<<"your Travel is"<<endl;
+			cout<<"From DIRE DAWA"<<endl;
+			cout<<"To ADDIS ABEBA"<<endl;
+			cout<<"It is 453.5KM it takes 10:100 hrs "<<endl;
+            cout<<"It costs 1450 ETB"<<endl;
+                        }
+            else if(dep==8 && dis==0)
+			{
+            cout<<"your Travel is"<<endl;
+			cout<<"From JIMMA"<<endl;
+			cout<<"To ADDIS ABEBA"<<endl;
+			cout<<"It is 500KM it takes 10:00 hrs "<<endl;
+            cout<<"It costs 1550 ETB"<<endl;
+                        }
+            else cout<<"error";              
 	
-	
-	
+int y;
+cout<<"If You want to reserve anothetr ticket enter 1 0r 0 unless enter another number"<<endl;
+cin>>y;
+if(y==0 || y==1)
+		goto start;
+else 
+return 0;
 }
